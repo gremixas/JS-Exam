8327753e82a681178e7d6673f28dcabe1ceb84b5
gremixas <andrius.agafonovas@gmail.com>
2022-12-07 20:55:23 +0200

    Task 4 - done

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,18 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+import cars from './cars.json' assert {type: 'json'}
+
+// const ENDPOINT = 'cars.json'; // Tikiuosi nepyksite, kad nesinaudojau fetch'u :)
+const output = document.querySelector('#output');
+
+cars.forEach(car => {
+    let models = '';
+    car.models.forEach(model => {
+        models += `<div class="model">${model}</div>`
+    });
+    output.innerHTML += `<div class="card">
+                            <div class="brand">${car.brand}</div>
+                            <div class="models">${models}</div>
+                        </div>`;
+});
