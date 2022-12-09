2056e1bfb133b52288739fb69b1ce7725e11a780
gremixas <andrius.agafonovas@gmail.com>
2022-12-09 14:38:43 +0200

    Task 4 file import method changed to fetch(). Different style colors.

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,18 +8,37 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-import cars from './cars.json' assert {type: 'json'}
+// Apsigalvojau del importo, nes https://htmlpreview.github.io/ tokiu budu neload'ina cars.json
+// O parsisiuntus projekta i PC jo neatidaro del CORS ...
+// import cars from './cars.json' assert {type: 'json'}
 
-// const ENDPOINT = 'cars.json'; // Tikiuosi nepyksite, kad nesinaudojau fetch'u :)
+const ENDPOINT = 'cars.json';
 const output = document.querySelector('#output');
 
-cars.forEach(car => {
-    let models = '';
-    car.models.forEach(model => {
-        models += `<div class="model">${model}</div>`
+async function readJsonFile(file) {
+    try {
+        const response = await fetch(file);
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.error('Something went wrong while opening data file.');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+readJsonFile(ENDPOINT).then(cars => {
+    cars.forEach(car => {
+        let models = '';
+        car.models.forEach(model => {
+            models += `<div class="model">${model}</div>`
+        });
+        output.innerHTML += `<div class="card">
+                                <div class="brand">${car.brand}</div>
+                                <div class="models">${models}</div>
+                            </div>`;
     });
-    output.innerHTML += `<div class="card">
-                            <div class="brand">${car.brand}</div>
-                            <div class="models">${models}</div>
-                        </div>`;
 });
+
